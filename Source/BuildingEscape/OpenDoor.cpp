11764975fa5fe2a08317a19095daa07cdbd95292
Edwin Villanueva <edwincv0@gmail.com>
2022-04-15 09:23:50 -0700

    Start Animating.

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Copyright Edwin Villanueva 2022.
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
+	UE_LOG(LogTemp, Warning, TEXT("Yaw is: %f"), GetOwner()->GetActorRotation().Yaw);
+	UE_LOG(LogTemp, Warning, TEXT("%f"), TargetYaw);
+
+	float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+	float NewYaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.01);
+	
+	FRotator OpenDoor{0.0f, NewYaw, 0.0f};
+	GetOwner()->SetActorRotation(OpenDoor);
+}
+
